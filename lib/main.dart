aba192e399bff5bbeb695b552d533590dcfb534a
Igor Pedroso <iigorpedroso@gmail.com>
2021-01-24 18:22:17 -0300

    configuracoes principais do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:jogo_velha/views/game_view.dart';
+import 'package:jogo_velha/views/main_view.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-        home: Container(),
-      )
-
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MainView(),
+      'game': (context) => GameView(),
+    },
+    theme: ThemeData(
+      primaryColor: Color(0xff2a1b52)
+    ),
+  ));
 }
-
